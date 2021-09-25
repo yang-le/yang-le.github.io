d1ac320f0b206611b1407607ca6d08c7a79a0bed
Yang Le <yangle0125@qq.com>
2021-09-25 16:13:06 +0800

    update

--- main.js
+++ main.js
@@ -12,7 +12,26 @@
             }
             return '';
         }
-    }).use(markdownitFootnote).use(markdownitAbbr).use(markdownitEmoji).use(markdownItKatexx).use(markdownItAnchor).use(markdownItTocDoneRight, { placeholder: '\\@\\[TOC\\](\\(.*\\))?' });
+    }).use(markdownitFootnote).use(markdownitAbbr).use(markdownitEmoji).use(markdownitContainer, 'container', {
+        validate: function (params) {
+            return true;
+        },
+        render: function (tokens, idx) {
+            if (tokens[idx].nesting === 1) {
+                const params = tokens[idx].info.trim();
+                if (params === '') return '<div class="container">';
+
+                const i = params.indexOf(' ');
+                if (i === -1) return `<div class="container ${params}">`;
+
+                const type = params.slice(0, i);
+                const title = params.slice(i);
+                return `<div class="container ${type}">` + (title ? `<p class="title">${title}</p>` : '');
+            } else {
+                return '</div>';
+            }
+        }
+    }).use(markdownItKatexx).use(markdownItAnchor).use(markdownItTocDoneRight, { placeholder: '\\@\\[TOC\\](\\(.*\\))?' });
 
     fetch('https://yang-le.github.io/articles/' + (article ? article : 'home') + '.md')
         .then(response => {
